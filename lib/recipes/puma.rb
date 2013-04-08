c97240572a0578d8bae54ec0aa4f714bf305f130
Leif Ringstad <leifcr@gmail.com>
2013-04-08 13:54:07 +0200

    Update puma scripts

--- lib/recipes/puma.rb
+++ lib/recipes/puma.rb
@@ -4,13 +4,15 @@ Capistrano::Configuration.instance.load do
 
   # The wrapped bin to start puma (bundle exec power...)
   set :puma_bin, 'bundle exec puma' unless exists?(:puma_bin)
+  set :puma_control, 'bundle exec pumactl' unless exists?(:puma_control)
   set :puma_socket, ("unix://" + File.join("#{shared_path}","sockets", "puma.sock")) unless exists?(:puma_socket)
 
   # Defines where the pid will live.
   set(:puma_pid) { File.join(pids_path,"#{app_server}.pid") } unless exists?(:puma_pid)
-  set(:puma_state) { File.join(shared_path, "puma.state") } unless exists?(:puma_state)
+  set(:puma_state) { File.join(shared_path, "sockets", "puma.state") } unless exists?(:puma_state)
+  set(:puma_control_url) { "unix://" + File.join(shared_path, "sockets", "pumactl.sock") } unless exists?(:puma_control_url)
 
-  set :puma_activate_control_app, false unless exists?(:puma_activate_control_app)
+  set :puma_activate_control_app, true unless exists?(:puma_activate_control_app)
 
   set :puma_on_restart_active, true unless exists?(:puma_on_restart_active)
   # Our puma template to be parsed by erb
@@ -21,18 +23,20 @@ Capistrano::Configuration.instance.load do
   # The remote location of puma's config file. Used by bluepill to fire it up
   set(:puma_remote_config) { File.join(shared_path, "config", "puma.rb") }
 
-  def puma_get_status
-    #"echo TODO fix curl to get status of puma server?"
+  def puma_status_cmd
+    "cd #{current_path} && #{puma_control} -S #{puma_state} status"
   end
   
   def puma_stop_cmd
-    #TODO
-    #"kill -QUIT `cat #{puma_pid}`"
+    "cd #{current_path} && #{puma_control} -S #{puma_state} stop"
   end
   
   def puma_restart_cmd
-    #TODO
-    #"kill -USR2 `cat #{puma_pid}`"
+    "cd #{current_path} && #{puma_control} -S #{puma_state} restart"
+  end
+
+  def puma_start_cmd
+    "cd #{current_path} && #{puma_bin} -C #{puma_remote_config}"
   end
 
   # Puma 
@@ -52,7 +56,17 @@ Capistrano::Configuration.instance.load do
     task :restart, :roles => :app do
       run puma_restart_cmd
     end
-    
+
+    desc "|capistrano-recipes| Flush puma sockets"
+    task :flush_sockets, :roles => :app do
+      run "rm -f #{File.join(shared_path, "sockets", "puma.sock")}"
+      run "rm -f #{File.join(shared_path, "sockets", "pumactl.sock")}"
+    end
+   
+    desc "|capistrano-recipes| Shows puma status"
+    task :status, :roles => :app do
+      run puma_status_cmd
+    end
     # ???????????????
     # desc "|capistrano-recipes| Tail puma log file" 
     # task :tail, :roles => :app do
@@ -68,18 +82,20 @@ Capistrano::Configuration.instance.load do
     up the puma.
     EOF
     task :setup, :roles => :app , :except => { :no_release => true } do
-      # TODO: refactor this to a more generic setup task once we have more socket tasks.
-      # commands = []
-      # commands << "mkdir -p #{sockets_path}"
-      # commands << "chown #{user}:#{group} #{sockets_path} -R" 
-      # commands << "chmod +rw #{sockets_path}"
+      puma.flush_sockets
+      commands = []
+      commands << "mkdir -p #{sockets_path}"
+      commands << "chown #{user}:#{group} #{sockets_path} -R" 
+      commands << "chmod +rw #{sockets_path}"
       
-      # run commands.join(" && ")
+      run commands.join(" && ")
       generate_config(puma_local_config, puma_remote_config)
     end
   end
   
   after 'deploy:setup' do
-    puma.setup if Capistrano::CLI.ui.agree("Create puma configuration file? [Yn]")
+    if Capistrano::CLI.ui.agree("Create puma configuration file? [Yn]")
+      puma.setup
+    end
   end if is_using('puma',:app_server)
 end
